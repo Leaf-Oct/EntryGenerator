3be2b98c8761ebfecd17d47e9ab8e4bf52ea9450
十月叶 <74179287+Leaf-Oct@users.noreply.github.com>
2023-07-14 11:29:41 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Application.launch(Window.class, args);
+	}
+
+}
